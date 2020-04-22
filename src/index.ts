618f5fc86129268a6348f29db955464df0763626
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2020-04-23 00:10:44 +0200

    chore(portable-text-editor): Export keyGenerator

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
+import {randomKey} from './utils/randomKey'
 export {PortableTextEditor} from './editor/PortableTextEditor'
 export * from './types/editor'
 export * from './types/portableText'
 export * from './types/schema'
 export {compactPatches} from './utils/patches'
 export {Patch} from './types/patch'
-
+export function keyGenerator() {
+  return randomKey(12)
+}
