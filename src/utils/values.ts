95939e86984f00bbab6cb8871bd1e07d4915c64f
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2020-04-23 00:06:47 +0200

    chore(portable-text-editor): Util for finding block and child

--- src/utils/values.ts
+++ src/utils/values.ts
@@ -1,5 +1,7 @@
+import {isEqual} from 'lodash'
 import {Node} from 'slate'
 import {PortableTextBlock} from '../types/portableText'
+import {PathSegment} from '../types/path'
 
 type Partial<T> = {
   [P in keyof T]?: T[P]
@@ -10,15 +12,14 @@ export function toSlateValue(
   textBlockType: string
 ): Node[] {
   if (value && Array.isArray(value)) {
-    return value
-      .map((blk: PortableTextBlock) => {
-        const {_type, _key, ...rest} = blk
-        const isPortableText = blk && blk._type === textBlockType
-        if (isPortableText) {
-          return {_type, _key, children: blk.children, ...rest}
-        }
-        return {_type, _key, children: [{text: ''}], value: rest}
-      })
+    return value.map((blk: PortableTextBlock) => {
+      const {_type, _key, ...rest} = blk
+      const isPortableText = blk && blk._type === textBlockType
+      if (isPortableText) {
+        return {_type, _key, children: blk.children, ...rest}
+      }
+      return {_type, _key, children: [{text: ''}], value: rest}
+    })
   }
   return []
 }
@@ -43,15 +44,48 @@ export function fromSlateValue(
 export function isEqualToEmptyEditor(children, portableTextFeatures) {
   return (
     children === undefined ||
-    children && Array.isArray(children) && children.length === 0 ||
-    children &&
-    Array.isArray(children) &&
-    (children.length === 1 &&
-      children[0]._type === portableTextFeatures.types.block.name &&
-      children[0].children &&
-      children[0].children.length === 1 &&
-      children[0].children[0]._type === 'span' &&
-      children[0].children[0].text === '')
+    (children && Array.isArray(children) && children.length === 0) ||
+    (children &&
+      Array.isArray(children) &&
+      children.length === 1 &&
+        children[0]._type === portableTextFeatures.types.block.name &&
+        children[0].children &&
+        children[0].children.length === 1 &&
+        children[0].children[0]._type === 'span' &&
+        children[0].children[0].text === '')
   )
+}
 
+export function findBlockAndIndexFromPath(
+  firstPathSegment: PathSegment,
+  children: Node[]
+): [Node | undefined, number] {
+  let blockIndex = -1
+  const isNumber = Number.isInteger(Number(firstPathSegment))
+  if (isNumber) {
+    blockIndex = Number(firstPathSegment)
+  } else {
+    blockIndex = children.findIndex(blk => isEqual({_key: blk._key}, firstPathSegment))
+  }
+  if (blockIndex > -1) {
+    return [children[blockIndex], blockIndex]
+  }
+  return [undefined, -1]
+}
+
+export function findChildAndIndexFromPath(
+  secondPathSegment: PathSegment,
+  block: Node
+): [Node | undefined, number] {
+  let childIndex = -1
+  const isNumber = Number.isInteger(Number(secondPathSegment))
+  if (isNumber) {
+    childIndex = Number(secondPathSegment)
+  } else {
+    childIndex = block.children.findIndex(child => isEqual({_key: child._key}, secondPathSegment))
+  }
+  if (childIndex > -1) {
+    return [block.children[childIndex], childIndex]
+  }
+  return [undefined, -1]
 }
