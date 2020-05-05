7df90df6f6d8b046f5ada8e35c6fef39c7b0e46a
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2020-05-06 01:22:26 +0200

    chore(portable-text-editor): Extend Slate's default normalization to add _type span to span inserted after a inline void object

--- src/editor/plugins/createWithSchemaTypes.ts
+++ src/editor/plugins/createWithSchemaTypes.ts
@@ -1,6 +1,8 @@
-import {Editor, Element} from 'slate'
+import {Editor, Element, Operation} from 'slate'
 import {PortableTextFeatures} from '../../types/portableText'
+import {debugWithName} from '../../utils/debug'
 
+const debug = debugWithName('plugin:withSchemaTypes')
 /**
  * This plugin makes sure that shema types are recognized properly by Slate as blocks, voids, inlines
  *
@@ -15,9 +17,19 @@ export function createWithSchemaTypes(portableTextFeatures: PortableTextFeatures
     }
     editor.isInline = (element: Element): boolean => {
       const inlineSchemaTypes = portableTextFeatures.types.inlineObjects.map(obj => obj.name)
-      return (
-        inlineSchemaTypes.includes(element._type) && element.__inline === true
-      )
+      return inlineSchemaTypes.includes(element._type) && element.__inline === true
+    }
+    // Extend Slate's default normalization to add _type span to span inserted after a inline void object
+    const {apply} = editor
+    editor.apply = (op: Operation) => {
+      const isInsertTextWithoutType =
+        op.type === 'insert_node' && op.path.length === 2 && op.node._type === undefined
+      if (isInsertTextWithoutType) {
+        const newNode = {...op.node, _type: portableTextFeatures.types.span.name}
+        op.node = newNode
+        debug('Setting span type to child without a type', op)
+      }
+      apply(op)
     }
     return editor
   }
