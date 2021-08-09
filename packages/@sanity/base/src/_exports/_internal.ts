335937c38ab791875d387f9476207252d5fb835a
Bjørge Næss <bjoerge@gmail.com>
2021-08-09 19:57:55 +0200

    fix(base): export every symbol from structure builder

--- packages/@sanity/base/src/_exports/_internal.ts
+++ packages/@sanity/base/src/_exports/_internal.ts
@@ -3,6 +3,8 @@ export {createWeightedSearch} from '../search/weighted/createWeightedSearch'
 
 export {default as FieldStatus} from '../__legacy/@sanity/components/fieldsets/FieldStatus'
 
+export {resizeObserver} from '../util/resizeObserver'
+
 export {RemoteSnapshotVersionEvent} from '../datastores/document/document-pair/checkoutPair'
 export {DocumentRemoteMutationEvent} from '../datastores/document/buffered-doc/types'
 export {remoteSnapshots} from '../datastores/document/document-pair/remoteSnapshots'
