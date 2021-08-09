849921cce29e11740d2ce607e480b0269eed2c5b
Bjørge Næss <bjoerge@gmail.com>
2021-08-09 11:32:49 +0200

    chore(base): add package exports folder

---
+++ packages/@sanity/base/src/_exports/_internal.ts
@@ -0,0 +1,8 @@
+export {observeForPreview} from '../preview'
+export {createWeightedSearch} from '../search/weighted/createWeightedSearch'
+
+export {default as FieldStatus} from '../__legacy/@sanity/components/fieldsets/FieldStatus'
+
+export {RemoteSnapshotVersionEvent} from '../datastores/document/document-pair/checkoutPair'
+export {DocumentRemoteMutationEvent} from '../datastores/document/buffered-doc/types'
+export {remoteSnapshots} from '../datastores/document/document-pair/remoteSnapshots'
